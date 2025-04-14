0ed5a263b3cd938238ae345db9963d157ae3d273
austinbutler <austinbutler@users.noreply.github.com>
2025-04-14 11:28:14 -0700

    Add Nix Flake (#2)

--- README.md
+++ README.md
@@ -1,55 +1,101 @@
 # FPS Conform
 
-:warning: Works for me but not tested much! I've used this on Arch Linux with files that have stereo and 5.1 audio.
+:warning: Works for me but not tested much! I've used this on Arch Linux with
+files that have stereo and 5.1 audio.
 
 Bash script to conform between NTSC and PAL.
 
 ## What's conforming?
 
-Conforming is either speeding up or slowing down the duration to match the desired output framerate. This is generally how films and TV shows are converted between standards.
+Conforming is either speeding up or slowing down the duration to match the
+desired output frame rate. This is generally how films and TV shows are
+converted between standards.
 
-The alternative is repeating or dropping frames to change the frame rate, and that's bad.
+The alternative is repeating or dropping frames to change the frame rate, and
+that's bad.
 
 ## Any change in quality?
 
 ### Video
 
-No. For the video this is done losslessly without re-encoding. The duration will be longer moving from PAL to NTSC and vice-versa.
+No. For the video this is done losslessly without re-encoding. The duration
+will be longer moving from PAL to NTSC and vice versa.
 
 ### Audio
 
-Yes. For the audio it is changing the tempo. This means the pitch is adjusted accordingly to match the new speed. In other words, there should be little perceptible difference in pitch between the original and the conformed one. This is a lossy process.
+Yes. For the audio it is changing the tempo. This means the pitch is adjusted
+accordingly to match the new speed. In other words, there should be little
+perceptible difference in pitch between the original and the conformed one.
+This is a lossy process.
 
-The audio is re-encoded with Opus at 48000Hz/128K. This is actually very good quality for an Opus file.
+The audio is re-encoded with [Opus](https://opus-codec.org) at 48000Hz/128K.
+This is actually very good quality for an Opus file.
+
+## Running
+
+Provide the folder where your MKV videos to be converted are followed by the
+frame rate to conform to. Valid frame rates are `23.976`, `24`, and `25`.
+
+It _should_ not overwrite your originals, however it may be safest to _make a
+backup copy_!
+
+The first embedded subtitle will be used, otherwise if there's an SRT subtitle
+of the same name next to the video file it will use that.
+
+It will output the finished files in a `conformed` folder in the current
+working directory.
+
+### Nix
+
+If you have Nix installed with [Flakes enabled](https://wiki.nixos.org/wiki/Flakes)
+you can run the script like this:
+
+```shell
+nix run github:austinbutler/fps_conform [folder] [frame rate]
+```
+
+This is nice because you don't need to worry about installing any requirements.
+
+### Standalone
+
+You can also just clone this repo and run the script directly if you have
+all the necessary requirements installed (see below).
+
+```shell
+./fps_conform.sh [folder] [frame rate]
+```
 
 ## Requirements
 
 ### Not Included
 
-#### mkvmerge
+The following is only relevant if not running with Nix.
 
-Part of the [`mkvtoolnix`](https://www.bunkus.org/videotools/mkvtoolnix/) package usually. Used to change the video framerate.
+#### `mkvmerge`
 
-#### ffmpeg and ffprobe
+Part of the [`mkvtoolnix`](https://mkvtoolnix.download/) package usually. Used
+to change the video frame rate.
 
-Both usually come with [`ffmpeg`](https://www.ffmpeg.org/). Uses `ffprobe` to get the framerate of the input file and `ffmpeg` to convert the audio while maintaining pitch.
+#### `ffmpeg` and `ffprobe`
+
+Both usually come with [`ffmpeg`](https://ffmpeg.org). Uses `ffprobe` to get
+the frame rate of the input file and `ffmpeg` to convert the audio while
+maintaining pitch.
 
 ### Included
 
-#### srtshift
+#### `srtshift`
 
-Part of [`mplayer-tools`](http://mplayer-tools.sourceforge.net/). I couldn't even find a package for this in Arch! So since it's a simple Perl script it's just included. Used to convert the frame rate for SRT subtitles.
+Part of [`mplayer-tools`](http://mplayer-tools.sourceforge.net). I couldn't
+even find a package for this in Arch! So since it's a simple Perl script it's
+just included. Used to convert the frame rate for SRT subtitles.
 
-## Running
+## Test
 
-Provide the folder where your MKV videos to be converted are followed by the framerate to conform to. Valid framerates are 23.976, 24, and 25.
+There's a rudimentary sanity-check test that can be run.
 
-It *should* not overwrite your originals, however it may be safest to _make a backup copy_!
-
-The first embedded subtitle will be used, otherwise if there's an SRT subtitle of the same name next to the video file it will use that.
-
-```
-bash fps_conform.sh [folder] [framerate]
+```shell
+just test
 ```
 
-This will output the finished files in a `converted` folder in the same directory as the script.
+The test clip is from [Sprite Fright](https://studio.blender.org/projects/sprite-fright).
---
+++ flake.nix
@@ -0,0 +1,151 @@
+{
+  description = "Conform between 23.976/24fps and 25fps";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.flake = false;
+    git-hooks.url = "github:cachix/git-hooks.nix";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      git-hooks,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = { };
+          overlays = [ (final: prev: { }) ];
+        };
+        pre-commit-check = git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            check-case-conflict = {
+              enable = true;
+              name = "check for case conflicts";
+              description = "checks for files that would conflict in case-insensitive filesystems.";
+              entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/check-case-conflict";
+            };
+            check-executables-have-shebangs = {
+              enable = true;
+              name = "check that executables have shebangs";
+              description = "ensures that (non-binary) executables have a shebang.";
+              entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/check-executables-have-shebangs";
+              types = [
+                "text"
+                "executable"
+              ];
+            };
+            check-merge-conflict = {
+              enable = true;
+              name = "check for merge conflicts";
+              description = "checks for files that contain merge conflict strings.";
+              entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/check-merge-conflict";
+              types = [ "text" ];
+            };
+            check-shebang-scripts-are-executable = {
+              enable = true;
+              name = "check that scripts with shebangs are executable";
+              description = "ensures that (non-binary) files with a shebang are executable.";
+              entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/check-shebang-scripts-are-executable";
+              types = [ "text" ];
+            };
+            check-vcs-permalinks = {
+              enable = true;
+              name = "check vcs permalinks";
+              description = "ensures that links to vcs websites are permalinks.";
+              entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/check-vcs-permalinks";
+              types = [ "text" ];
+            };
+            detect-private-key = {
+              enable = true;
+              name = "detect private key";
+              description = "detects the presence of private keys.";
+              entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/detect-private-key";
+              types = [ "text" ];
+            };
+            end-of-file-fixer = {
+              enable = true;
+              name = "fix end of files";
+              description = "ensures that a file is either empty, or ends with one newline.";
+              entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/end-of-file-fixer";
+              types = [ "text" ];
+            };
+            mixed-line-ending = {
+              enable = true;
+              name = "mixed line ending";
+              description = "replaces or checks mixed line ending.";
+              entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/mixed-line-ending --fix=lf";
+              types = [ "text" ];
+            };
+            nixfmt-rfc-style.enable = true;
+            nil.enable = true;
+            prettier = {
+              enable = true;
+              types = [ "markdown" ];
+            };
+            shellcheck = {
+              enable = true;
+              types_or = nixpkgs.lib.mkForce [ "shell" ];
+            };
+            shfmt.enable = true;
+            statix.enable = true;
+            trailing-whitespace = {
+              enable = true;
+              name = "trim trailing whitespace";
+              description = "trims trailing whitespace.";
+              entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/trailing-whitespace-fixer";
+              types = [ "text" ];
+            };
+          };
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "fps_conform";
+
+          buildInputs = with pkgs; [
+            ffmpeg
+            git
+            just
+            mkvtoolnix-cli
+            nixfmt-rfc-style
+            nodePackages.prettier
+            perl
+            pre-commit
+            shellcheck
+            shfmt
+          ];
+
+          shellHook = ''
+            ${pre-commit-check.shellHook}
+          '';
+        };
+        packages.default = pkgs.writeShellApplication {
+          name = "fps-conform";
+          runtimeInputs = with pkgs; [
+            (stdenv.mkDerivation {
+              name = "srtshift";
+              src = ./srt;
+              buildPhase = "";
+              installPhase = ''
+                mkdir -p $out/bin
+                cp srtshift.pl $out/bin/srtshift
+              '';
+            })
+            ffmpeg
+            mkvtoolnix-cli
+            perl
+          ];
+          text = builtins.readFile ./fps_conform.sh;
+        };
+      }
+    );
+}
